b75b154a28f5e48a9c4208a754ae6dbba789629e
Charles ASSOGBA <charlesclaudepascal@gmail.com>
2026-01-23 10:38:40 +0800

    Fix the burger menu bug

--- frontend/src/Components/ResponsiveMenu.jsx
+++ frontend/src/Components/ResponsiveMenu.jsx
@@ -5,11 +5,11 @@ const ResponsiveMenu = ({open}) => {
     <AnimatePresence mode='wait'>
         {
         open && (
-          <motion.div
+          <motion
            initial={{opacity: 0, y: -100}}
-           animate={{opacity: 1, y: 0}}
+           animate={{opacity: 2, y: 0}}
            exit={{opacity: 0, y: -100}}
-           transition={{type: 'tween', duration: 0.3}}
+           transition={{type: 'tween', duration: 0.2}}
            className="absolute top-20 left-0 w-full h-screen z-20">
             <div className="text-xl font-semibold uppercase bg-primary text-white py-10 m-6 rounded-3xl">
                <ul className="flex flex-col justify-center items-center gap-10">
@@ -21,7 +21,7 @@ const ResponsiveMenu = ({open}) => {
                 <li>Electronics</li>
                </ul>
             </div>
-            </motion.div>
+            </motion>
         )
         }
     </AnimatePresence>
